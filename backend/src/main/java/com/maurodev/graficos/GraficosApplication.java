4764535db78b2d9febd7f2699bf33a1a1c8e483e
MauroDegaspari <mauro_degaspari@hotmail.com>
2021-11-02 15:05:14 -0300

    subindo projeto inicial

---
+++ backend/src/main/java/com/maurodev/graficos/GraficosApplication.java
@@ -0,0 +1,13 @@
+package com.maurodev.graficos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraficosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraficosApplication.class, args);
+	}
+
+}
