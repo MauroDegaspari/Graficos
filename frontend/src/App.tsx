2be8f6a8335a82b39a5ae4a4e7cf9050ca50707a
MauroDegaspari <mauro_degaspari@hotmail.com>
2021-11-03 11:01:01 -0300

    Add apexCharts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import DataTable from 'components/DataTable';
 import Footer from 'components/Footer';
+import GraficoBarras from 'components/GraficoBarras';
+import GraficoPizza from 'components/GraficoPizza';
 import NavBar from 'components/NavBar';
 
 
@@ -8,10 +10,24 @@ function App() {
   <>
   <NavBar/>
   < div className="container">
-      <h1 className="text-primary"> Grafico de contas</h1> 
+      <h1 className="text-primary"> Dashboard</h1>
+
+          <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">VendasBarras</h5>
+              <GraficoBarras></GraficoBarras>
+            </div>
+          
+
+           <div className="col-sm-6">
+              <h5 className="text-center text-secondary">VendasPizza</h5>
+              <GraficoPizza></GraficoPizza>
+            </div>
+       
+           </div> 
       <DataTable/>
      
-    </div>
+      </div>
     <Footer/>
   </>
   );
