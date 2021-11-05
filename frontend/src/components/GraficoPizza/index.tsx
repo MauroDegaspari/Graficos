28238662a3af8e6ba0ad0d87f647a36e06da1b25
MauroDegaspari <mauro_degaspari@hotmail.com>
2021-11-05 17:32:23 -0300

    integraçao de dados do backend com o front AXIOS no graficoPizza

--- frontend/src/components/GraficoPizza/index.tsx
+++ frontend/src/components/GraficoPizza/index.tsx
@@ -1,11 +1,38 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { BASE_URL } from 'utils/requests';
+import { SomaVendas } from 'types/vendas';
+import { useEffect, useState } from 'react';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 function GraficoPizza(){
+
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series: []})
+  
+    useEffect(() => {
+
+        // tratamento de dados
+    axios.get(BASE_URL + '/vendas/total-vendas')
+    .then(response => { 
+        const data = response.data as SomaVendas[];
+        const MeusLabels = data.map(x => x.nomeVendedor);
+        const MeusSeries = data.map(x => x.total);
+
+        setChartData({labels: MeusLabels, series: MeusSeries})
+        //console.log(response.data)
+    });
+
+    } , []);  
     
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    
+   // const mockData = {
+   //   series: [477138, 499928, 444867, 220426, 473088],
+   //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+   //}
     
     const options = {
         legend: {
@@ -15,8 +42,8 @@ function GraficoPizza(){
     
     return(
         <Chart
-                options={{...options, labels: mockData.labels}}
-                series={mockData.series}
+                options={{...options, labels: chartData.labels}}
+                series={chartData.series}
                 type="donut"
                 height="240"
 
---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,2 @@
+//export const BASE_URL = 'http://localhost:8087'
+export const BASE_URL = 'https://graficosvendas-backend.herokuapp.com'
