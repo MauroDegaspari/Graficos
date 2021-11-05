459173be276a8dd55508e8e0ca1d645f18e0588e
MauroDegaspari <mauro_degaspari@hotmail.com>
2021-11-05 12:03:11 -0300

    atualizaçao tela inicial HOME

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return(
-        <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-4 bg-dark">
         <div className="container">
           <p className="text-light">App desenvolvido por <a href="https://github.com/maurodegaspari" target="_blank" rel="noreferrer">Mauro Degaspari</a></p>
           <p className="text-light"><small><strong>Semana Spring React</strong><br/>
--- frontend/src/page/Home/index.tsx
+++ frontend/src/page/Home/index.tsx
@@ -1,12 +1,42 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 
 function Home(){
     return(
-    <div>
-        <>
-          <h2> pagina inicial</h2>
-        </>
-    </div>
+        <div>
+            <>
+            <NavBar/>
+
+                <div className="container">
+                <div className="row">
+                    <div className="col">
+                        
+                        <div className="jumbotron container">
+                            <h1 className="display-4">Olá, mundo!</h1>
+                            <p className="lead">Este é um simples componente jumbotron para chamar mais atenção a um determinado conteúdo ou informação.</p>
+                            <hr className="my-4"/>
+                            <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>           
+                            <a className="btn btn-primary btn-lg" href="/Dashboard" role="button">Acesse Aqui</a>
+                        </div>
+                     </div>
+
+                    <div className="col container">
+                        <div className="card">
+                        <img className="card-img-top" src="/src/img/eu.jpeg" alt="Imagem de capa do card"/>
+                        <div className="card-body">
+                        <h5 className="card-title">Meu GitHub</h5>
+                        <p className="card-text">Acesse meu GitHub para poder ter mais conhecimento das minhas Skill e outros Repositorios.</p>
+                        <a href="https://github.com/MauroDegaspari" className="btn btn-primary">Visitar</a>
+                        </div>
+                        </div>
+                    </div>
+                 </div>
+                 </div>  
+              <Footer/>
+            </>
+        </div>
+        
     )
 }
 
